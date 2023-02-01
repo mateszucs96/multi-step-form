80a25707d02ecf22ae16776347d36b115b59880e
mateszucs96 <szucsmate096@gmail.com>
2023-02-01 20:15:01 +0100

    add checkboxes to AddOns step

--- src/components/AddOns.tsx
+++ src/components/AddOns.tsx
@@ -9,6 +9,27 @@ const AddOns = ({ step }: { step: number }) => {
 				title="Pick add-ons"
 				info="Add-ons help enhance your gaming experience."
 			/>
+			<div className={`${styles.onlineService} checkboxContainer`}>
+				<input type="checkbox" id="scales" name="scales" />
+				<label htmlFor="scales">
+					<h3>Online service</h3>
+					<p>Access to multiplayer games</p>
+				</label>
+			</div>
+			<div className={`${styles.largerStorage} checkboxContainer`}>
+				<input type="checkbox" id="scales" name="scales" />
+				<label htmlFor="scales">
+					<h3>Larger storage</h3>
+					<p>Extra 1TB of cloud save</p>
+				</label>
+			</div>
+			<div className={`${styles.customProfile} checkboxContainer`}>
+				<input type="checkbox" id="scales" name="scales" />
+				<label htmlFor="scales">
+					<h3>Customizable profile</h3>
+					<p>Custom theme on your profile</p>
+				</label>
+			</div>
 		</section>
 	);
 };
